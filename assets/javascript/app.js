6f7a3016dd6e5b3a1a6d786bd658f086d0dedb03
ivancho277 <rouskoi@live.com>
2019-03-08 07:19:46 -0800

    just pushed question objects to array

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -20,7 +20,7 @@
 
 //Class to build each question
 class Question {
-    constructor(question, answ1, answ2, answ3, answ4, correctAnsw){
+    constructor(question, answ1, answ2, answ3, answ4, correctAnsw) {
         this.question = question;
         this.answ1 = answ1;
         this.answ2 = answ2;
@@ -29,38 +29,41 @@ class Question {
         this.correctAnsw = correctAnsw;
     }
 
-    questionTimer(){
-       let timer = 30;
-       let intervalId;
-       function decrement() {
-           timer--;
-           $("#time").text("Time left: " + timer);
-           if(timer === 0){
-            stop();
-            alert("out of time");
+    questionTimer() {
+        let timer = 30;
+        let intervalId;
+
+        function decrement() {
+            timer--;
+            $("#time").text("Time left: " + timer);
+            if (timer === 0) {
+                stop();
+                alert("out of time");
+            }
         }
-       }
-       function stop(){
-           clearInterval(intervalId);
-           
-       }
-       function run(){
-           clearInterval(intervalId);
-           intervalId = setInterval(decrement, 1000);
-       }
-       run();
+
+        function stop() {
+            clearInterval(intervalId);
+
+        }
+
+        function run() {
+            clearInterval(intervalId);
+            intervalId = setInterval(decrement, 1000);
+        }
+        run();
     }
 
     //Print to screen
-    printToScreen(){
-        $("#question-id").text( this.question);
+    printToScreen() {
+        $("#question-id").text(this.question);
         $("#answer-a").text("a. " + this.answ1);
         $("#answer-b").text("b. " + this.answ2);
         $("#answer-c").text("c. " + this.answ3);
         $("#answer-d").text("d. " + this.answ4);
     }
 
-    printQuestionToConsole(){
+    printQuestionToConsole() {
         console.log(this.question);
         console.log("a:", this.answ1);
         console.log("b:", this.answ2);
@@ -69,30 +72,34 @@ class Question {
         console.log(this.correctAnsw);
     }
 
-   
+
 
 }
-$(document).ready(function() {
+$(document).ready(function () {
 
 
 
-$("#time").hide()
-$("#game-section").hide()
-let Question1 = new Question("How many ski area are in Washington State?", "10", "16", "20", "31", "20");
-let Question2 = new Question("What is the closest ski resort to Seattle?", "Stevens Pass", "Snoqualmie Pass", "Mt. Baker", "Hurrican Ridge", "Snoqualmie Pass")
-let Question3 = new Question("What ski resort holds the record for most snowfall in a single season for the USA(at 1,140inches)?", "Alta, UT", "Mt. Baker, WA", "Timberline, OR", "Alyeska, AK", "Mt. Baker, WA")
-let Question4 = new Question("Which ski Resort is NOT in Idaho?", "Sun Valley", "Bogus Basin", "Jackson Hole", "Targhee","Jackson Hole")
-let Question5 = new Question("What is the biggest ski Resort in North America?", "Whistler-Blackcomb", "Big Sky", "Park City", "Heavenly", "Whistler-Blackcomb")
-let Question6 = new Question("Which ski resort is NOT in Colorado?", "Crested Butte", "Whitefish", "Breckenridge", "Steamboat", "Whitefish")
-let Question7 = new Question("How high is Chacaltaya Bolivia, the highest ski resort in the world?", "20,110ft.", "17,785ft", "13,235ft", "15,000ft", "17,785ft")
-let Question8 = new Question("What male skier holds the most X-Games medals?", "Tanner Hall", "Simon Dumont", "Tom Wallisch", "Gus Kenworthy", "Tanner Hall")
-let Question9 = new Question("What skier has an infamos video of breaking both ankles?", "Gus Kenworthy", "James Woods","Candide Thorvex","Tanner Hall","Tanner Hall")
+    $("#time").hide()
+    $("#game-section").hide()
+    let Question1 = new Question("How many ski area are in Washington State?", "10", "16", "20", "31", "20");
+    let Question2 = new Question("What is the closest ski resort to Seattle?", "Stevens Pass", "Snoqualmie Pass", "Mt. Baker", "Hurrican Ridge", "Snoqualmie Pass")
+    let Question3 = new Question("What ski resort holds the record for most snowfall in a single season for the USA(at 1,140inches)?", "Alta, UT", "Mt. Baker, WA", "Timberline, OR", "Alyeska, AK", "Mt. Baker, WA")
+    let Question4 = new Question("Which ski Resort is NOT in Idaho?", "Sun Valley", "Bogus Basin", "Jackson Hole", "Targhee", "Jackson Hole")
+    let Question5 = new Question("What is the biggest ski Resort in North America?", "Whistler-Blackcomb", "Big Sky", "Park City", "Heavenly", "Whistler-Blackcomb")
+    let Question6 = new Question("Which ski resort is NOT in Colorado?", "Crested Butte", "Whitefish", "Breckenridge", "Steamboat", "Whitefish")
+    let Question7 = new Question("How high is Chacaltaya Bolivia, the highest ski resort in the world?", "20,110ft.", "17,785ft", "13,235ft", "15,000ft", "17,785ft")
+    let Question8 = new Question("What male skier holds the most X-Games medals?", "Tanner Hall", "Simon Dumont", "Tom Wallisch", "Gus Kenworthy", "Tanner Hall")
+    let Question9 = new Question("What skier has an infamos video of breaking both ankles?", "Gus Kenworthy", "James Woods", "Candide Thorvex", "Tanner Hall", "Tanner Hall")
 
-$("#start-btn").click(function(){
-    Question8.printToScreen()
-    Question8.questionTimer()
-    $("#game-section").show()
-    $("#time").show()
-    $("#start-btn").hide()
-})
+let questionArray = [Question1, Question2, Question3, Question5, Question6, Question7, Question8, Question9]
+      console.log(questionArray);
+
+
+    $("#start-btn").click(function () {
+        Question8.printToScreen()
+        Question8.questionTimer()
+        $("#game-section").show()
+        $("#time").show()
+        $("#start-btn").hide()
+    })
 })
