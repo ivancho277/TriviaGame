c51cb76e01e51f116ce978470305cc48a85465c1
ivancho277 <rouskoi@live.com>
2019-03-08 17:56:35 -0800

    got cycling of questions to work!

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -29,26 +29,27 @@ class Question {
         this.correctAnsw = correctAnsw;
         this.timeup = false;
         //this.questionTimer();
-        this.isAnsweredCorrect = false;
+        this.isAnsweredCorrect;
         //this.printQuestionToConsole();
-        
+
 
     }
-        
+
 
     static questionTimer() {
-        let timer = 30;
+        let timer = 100;
         let intervalId;
 
         function decrement() {
             timer--;
             $("#time").text("Time left: " + timer);
             if (timer === 0) {
-                stop();                
-                $("#game-section").html("<h1> Out of Time! <h1>")                
+                stop();
+                $("#game-section").html("<h1> Out of Time! <h1>")
                 this.timeup = true;
             }
         }
+
         function stop() {
             clearInterval(intervalId);
 
@@ -60,56 +61,53 @@ class Question {
         }
         run();
     }
-    
+
     //check if clicked answer is correct answer
     checkifClickedisCorrect() {
-        var ans1 = this.answ1  //my method of binding class variable to be visible in the click events
-        var ans2 = this.answ2
-        var ans3 = this.answ3
-        var ans4 = this.answ4
-        var correct = this.correctAnsw
-        
+
+
         $("#answer-a").click(() => {
             console.log(this.isAnsweredCorrect);
             console.log($(this));
-            if(ans1 === correct)
-            {
+            if (this.answ1 === this.correctAnsw) {
                 console.log("correct");
                 this.isAnsweredCorrect = true;
 
+            } else {
+                console.log("incorrect")
             }
-            else{console.log("incorrect")}
         })
         $("#answer-b").click(() => {
             console.log(this.isAnsweredCorrect);
-            if(ans2 === correct)
-            {
+            if (this.answ2 === this.correctAnsw) {
                 console.log("correct");
                 this.isAnsweredCorrect = true;
+            } else {
+                console.log("incorrect")
             }
-            else{console.log("incorrect")}
         })
         $("#answer-c").click(() => {
-            
-            if(ans3 === correct)
-            {
+
+            if (this.answ3 === this.correctAnsw) {
                 console.log("correct");
                 this.isAnsweredCorrect = true;
                 console.log(this.isAnsweredCorrect);
+            } else {
+                console.log("incorrect")
             }
-            else{console.log("incorrect")}
         })
         $("#answer-d").click(() => {
-            
-            if(ans4 === correct)
-            {
+
+            if (this.answ4 === this.correctAnsw) {
                 console.log("correct");
                 this.isAnsweredCorrect = true;
                 console.log(this.isAnsweredCorrect);
+            } else {
+                console.log("incorrect")
             }
-            else{console.log("incorrect")}
         })
-        
+
+
     }
 
     //Print to screen
@@ -131,24 +129,28 @@ class Question {
     }
 
 }
+
+let Question1 = new Question("How many ski area are in Washington State?", "10", "16", "20", "31", "20");
+let Question2 = new Question("What is the closest ski resort to Seattle?", "Stevens Pass", "Snoqualmie Pass", "Mt. Baker", "Hurrican Ridge", "Snoqualmie Pass")
+let Question3 = new Question("What ski resort holds the record for most snowfall in a single season for the USA(at 1,140inches)?", "Alta, UT", "Mt. Baker, WA", "Timberline, OR", "Alyeska, AK", "Mt. Baker, WA")
+let Question4 = new Question("Which ski Resort is NOT in Idaho?", "Sun Valley", "Bogus Basin", "Jackson Hole", "Targhee", "Jackson Hole")
+let Question5 = new Question("What is the biggest ski Resort in North America?", "Whistler-Blackcomb", "Big Sky", "Park City", "Heavenly", "Whistler-Blackcomb")
+let Question6 = new Question("Which ski resort is NOT in Colorado?", "Crested Butte", "Whitefish", "Breckenridge", "Steamboat", "Whitefish")
+let Question7 = new Question("How high is Chacaltaya Bolivia, the highest ski resort in the world?", "20,110ft.", "17,785ft", "13,235ft", "15,000ft", "17,785ft")
+let Question8 = new Question("What male skier holds the most X-Games medals?", "Tanner Hall", "Simon Dumont", "Tom Wallisch", "Gus Kenworthy", "Tanner Hall")
+let Question9 = new Question("What skier has an infamos video of breaking both ankles?", "Gus Kenworthy", "James Woods", "Candide Thorvex", "Tanner Hall", "Tanner Hall")
+
+
 $(document).ready(function () {
 
 
 
     $("#time").hide()
     $("#game-section").hide()
-    let Question1 = new Question("How many ski area are in Washington State?", "10", "16", "20", "31", "20");
-    let Question2 = new Question("What is the closest ski resort to Seattle?", "Stevens Pass", "Snoqualmie Pass", "Mt. Baker", "Hurrican Ridge", "Snoqualmie Pass")
-    let Question3 = new Question("What ski resort holds the record for most snowfall in a single season for the USA(at 1,140inches)?", "Alta, UT", "Mt. Baker, WA", "Timberline, OR", "Alyeska, AK", "Mt. Baker, WA")
-    let Question4 = new Question("Which ski Resort is NOT in Idaho?", "Sun Valley", "Bogus Basin", "Jackson Hole", "Targhee", "Jackson Hole")
-    let Question5 = new Question("What is the biggest ski Resort in North America?", "Whistler-Blackcomb", "Big Sky", "Park City", "Heavenly", "Whistler-Blackcomb")
-    let Question6 = new Question("Which ski resort is NOT in Colorado?", "Crested Butte", "Whitefish", "Breckenridge", "Steamboat", "Whitefish")
-    let Question7 = new Question("How high is Chacaltaya Bolivia, the highest ski resort in the world?", "20,110ft.", "17,785ft", "13,235ft", "15,000ft", "17,785ft")
-    let Question8 = new Question("What male skier holds the most X-Games medals?", "Tanner Hall", "Simon Dumont", "Tom Wallisch", "Gus Kenworthy", "Tanner Hall")
-    let Question9 = new Question("What skier has an infamos video of breaking both ankles?", "Gus Kenworthy", "James Woods", "Candide Thorvex", "Tanner Hall", "Tanner Hall")
 
-let questionArray = [Question1, Question2, Question3, Question5, Question6, Question7, Question8, Question9]
-      console.log(questionArray);
+
+    let questionArray = [Question1, Question2, Question3, Question5, Question6, Question7, Question8, Question9]
+    console.log(questionArray);
 
 
     $("#start-btn").click(function () {
@@ -156,14 +158,14 @@ let questionArray = [Question1, Question2, Question3, Question5, Question6, Ques
         Question1.checkifClickedisCorrect();
         Question.questionTimer();
 
-      
-     
+
+
 
         //display question1
         //if time runs out on question 1 go to question 2 etc...
 
 
-        
+
         // for(var i = 0; i < questionArray.length; i++)
         // {              
         //     {
@@ -176,15 +178,39 @@ let questionArray = [Question1, Question2, Question3, Question5, Question6, Ques
         $("#game-section").show()
         $("#start-btn").hide()
     })
-
-$('#answer-a, #answer-b, #answer-c, #answer-d').click(() => {
-    if(Question1.isAnsweredCorrect === true){
-        $("#game-section")
+    $('#answer-a, #answer-b, #answer-c, #answer-d').click(() => {
+        // $("#game-section")
         Question2.printToScreen();
-    }
-})
-    
+        $('#answer-a, #answer-b, #answer-c, #answer-d').click(() => {
+
+            Question3.printToScreen();
+
+            $('#answer-a, #answer-b, #answer-c, #answer-d').click(() => {
+                Question4.printToScreen();
+
+                $('#answer-a, #answer-b, #answer-c, #answer-d').click(() => {
+                    Question5.printToScreen()
+
+                    $('#answer-a, #answer-b, #answer-c, #answer-d').click(() => {
+                        Question6.printToScreen();
+                        $('#answer-a, #answer-b, #answer-c, #answer-d').click(() => {
+                            Question7.printToScreen();
+
+                            $('#answer-a, #answer-b, #answer-c, #answer-d').click(() => {
+                                Question8.printToScreen();
+                                $('#answer-a, #answer-b, #answer-c, #answer-d').click(() => {
+                                    Question9.printToScreen();
+                                })
+
+                            })
 
 
+                        })
 
+                    })
+                })
+            })
+
+        })
+    })
 })
